a395a0def9c0458bb618531a8f98be803fe1ebf9
Alain <alains214@gmail.com>
2020-10-17 19:03:19 -0400

    first commit

---
+++ frontend/quiz.js
@@ -0,0 +1,9 @@
+class Quiz{
+    constructor(question, choices, correct_choice, topic_id) {
+        this.question = question
+        this.choices = choices
+        this.correct_choice = correct_choice
+        this.topic_id = topic_id
+    }
+}
+
