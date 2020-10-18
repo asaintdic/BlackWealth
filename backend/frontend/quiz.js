666a39a7009b115bf5deacdacc101799ee89e225
Alain <alains214@gmail.com>
2020-10-17 22:11:41 -0400

    delete copy frontend

--- backend/frontend/quiz.js
+++
@@ -1,9 +0,0 @@
-class Quiz{
-    constructor(question, choices, correct_choice, topic_id) {
-        this.question = question
-        this.choices = choices
-        this.correct_choice = correct_choice
-        this.topic_id = topic_id
-    }
-}
-
--- frontend/topic.js
+++ frontend/topic.js
@@ -10,33 +10,6 @@ class Topic{
     }
 }
 
-const topicUrl = "http://localhost:3000/topics"
-
-document.addEventListener('DOMContentLoaded', (e) => {
-    console.log('DOM fully loaded'); 
-});
-
-function fetchTopic() {
-    fetch(topicUrl)
-    .then(res => res.json())
-    .then(topics => renderTopics(topics))
+renderTopic = () => {
+    const topicDiv = document.getElementById("topic-container")
 }
-
-function renderTopics(t) {
-    const div = document.getElementById('wealth-article')
-    
-     //title
-    const h1 = document.createElement('h1')
-    //author
-    const h4 = document.createElement('h4')
-    //body
-    const p = document.createElement('p')
-    //url
-    const url = document.createElement('url')
-}
-
-
-renderTopics()
-
-// .for each
-// const item = new Item
